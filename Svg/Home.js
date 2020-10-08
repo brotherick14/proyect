2777ee8ab62fa2ebb8abbb081468ef7746fe27da
ERICK JERONIMO <erickjeronimo1@gmail.com>
2020-10-08 12:31:17 -0600

    svg app

--- Svg/Home.js
+++ Svg/Home.js
@@ -1,26 +1,21 @@
-import * as React from "react"
-import Svg, { Defs, Path } from "react-native-svg"
-/* SVGR has dropped some elements not supported by react-native-svg: style, title */
+import * as React from "react";
 
 function SvgComponent(props) {
   return (
-    <Svg
-      id="prefix__Layer_1"
-      data-name="Layer 1"
-      viewBox="0 0 1200 1200"
-      {...props}
-    >
-      <Defs></Defs>
-      <Path
-        className="prefix__cls-1"
-        d="M1011.54 1147.37H195.08l-3.24-578.85 38.23-.22 3.03 540.83h739.95l-3.59-532.15 38.23-.26 3.85 570.65z"
+    <svg id="Layer_1" data-name="Layer 1" viewBox="0 0 1200 1200" {...props}>
+      <defs>
+        <style>{".cls-1{fill:#5883f0}"}</style>
+      </defs>
+      <path
+        className="cls-1"
+        d="M1011.54 1147.37L195.08 1147.37 191.84 568.52 230.07 568.3 233.1 1109.13 973.05 1109.13 969.46 576.98 1007.69 576.72 1011.54 1147.37z"
       />
-      <Path
-        className="prefix__cls-1"
+      <path
+        className="cls-1"
         d="M1143 594.44H44.06L601 52.44zM138.16 556.21h912.53L600.62 106.14z"
       />
-    </Svg>
-  )
+    </svg>
+  );
 }
 
-export default SvgComponent
+export default SvgComponent;
--- Svg/index.js
+++ Svg/index.js
@@ -1,2 +1,4 @@
 export {default as Likebuck } from './likebuck';
 export {default as Home } from './Home';
+export {default as Logo } from './Logo';
+export {default as Like } from './Like';
